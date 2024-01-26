7e36ebf1e1abb6d2153344bf3aa98a0b073df971
DRobinson4105 <drobinson4105@gmail.com>
2024-01-25 22:40:09 -0500

    Implemented Disjoint Set in Java

---
+++ Concepts/DataStructures/DisjointSet.java
@@ -0,0 +1,25 @@
+class DS {
+    int arr[];
+
+    public DS(int n) {
+        arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = -1;
+    }
+
+    // Returns the parent of the set containing x
+    public int find(int x) {
+        return (arr[x] < 0) ? x : (arr[x] = find(arr[x]));
+    }
+
+    // Merges the sets containing a and b
+    public void merge(int a, int b) {
+        int p1 = find(a);
+        int p2 = find(b);
+        
+        if (p1 == p2) return;
+
+        arr[p1] += arr[p2];
+        arr[p2] = p1;
+    }
+};
