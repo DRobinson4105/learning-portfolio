0c22fb3a84bffc4f8fb0c2c12fc5c6091b31aee9
DRobinson4105 <drobinson4105@gmail.com>
2024-02-16 21:20:36 -0500

    Implemented Quick Sort in Java

---
+++ Algorithms/SortingAlgorithms/QuickSort.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class QuickSort {
+    public static void sort(List<Integer> list, int low, int high) {
+        if (low >= high) return;
+
+        int mid = Partition(list, low, high);
+        
+        sort(list, low, mid-1);
+        sort(list, mid+1, high);
+    }
+
+    private static void swap(List<Integer> list, int i, int j) {
+        int tmp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, tmp);
+    }
+
+    private static int Partition(List<Integer> list, int low, int high) {
+        int idx = new Random().nextInt(high-low+1) + low;
+        swap(list, idx, low);
+        int partition = list.get(low);
+        
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= j && list.get(i) < partition) i++;
+            while (i <= j && list.get(j) >= partition) j--;
+
+            if (i < j) swap(list, i, j);
+        }
+        
+        swap(list, low, j);
+        return j;
+    }
+
+    private static boolean isSorted(List<Integer> list) {
+        for (int i = 1; i < list.size(); i++)
+            if (list.get(i) < list.get(i-1)) return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Random r = new Random();
+
+        for (int i = 0; i < 10; i++)
+            list.add(r.nextInt());
+
+        System.out.println(list);
+
+        RadixSort.sort(list);
+
+        System.out.println(list);
+        System.out.println(isSorted(list));
+    }
+}
