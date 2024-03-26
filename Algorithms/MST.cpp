bbafb93bc71026b361a5523bfb814c3ea5b508a7
David Robinson <drobinson4105@gmail.com>
2024-03-26 15:09:45 -0400

    Fixed MST

--- Algorithms/MST.cpp
+++ Algorithms/MST.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
-#include "../DataStructures/DisjointSet.hpp"
-
 using namespace std;
 
 typedef long long ll;
 typedef vector<ll> vl;
 typedef vector<vl> vvl;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+struct DS {
+	vi s;
+	DS(int n): s(n, -1) {}
+	int find(int i) { return s[i] < 0 ? i : s[i] = find(s[i]); }
+    bool merge(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return false;
+		if (s[a] > s[b]) swap(a, b);
+		s[a] += s[b], s[b] = a;
+		return true;
+	}
+	int size(int i) { return -s[find(i)]; }
+	bool same(int a, int b) { return find(a) == find(b); }
+};
 
 int mst(vvl edges, int V) {
     DS ds(edges.size());
